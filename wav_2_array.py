7337813046c3305215e3615d2511b30c1b22b4ae
Henrique Bacelar <bacelarhenrique8@gmail.com>
2019-08-18 00:01:32 +0200

    upload embeddings

--- wav_2_array.py
+++ wav_2_array.py
@@ -3,12 +3,12 @@ import glob, os
 from tqdm import tqdm
 
 
-wav_path    = 'data/audio/snore_3s/'
+wav_path    = 'data/audio/bg_3s/'
 np_path     = 'data/arrays/'
 
 cmd         = 'python vggish/vggish_inference_demo.py --wav_file {} --numpy_file {} --checkpoint vggish/vggish_model.ckpt'
 wavs        = glob.glob(wav_path + '*.wav')
 
-for w in tqdm(wavs[0:5]):
+for w in tqdm(wavs):
     np_name = w.split('/')[-1].split('.')[0]
     _ = os.system(cmd.format(w, os.path.join(np_path, np_name)))
